d6e13f470746d3011b9f962f5447eeac33c9cdb0
HeroFactory16 <cedric.coinlag@gmail.com>
2024-04-05 16:07:00 +0200

    Local player health points display

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -4,18 +4,50 @@ using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 
-public class Health : NetworkBehaviour
+public class Health: MonoBehaviour
 {
+    public NetPlayerController player;
+    public int health;
     public TextMeshProUGUI healthText;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //public override void OnNetworkSpawn()
+    //{
+    //    base.OnNetworkSpawn();
+    //    if (IsClient)
+    //    {
+    //        player = FindObjectOfType<NetPlayerController>();
+    //    }
+    //}
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player != null)
+        {
+            UpdateLife();
+            UpdateLifeText();
+        }
+        else
+        {
+            player = FindObjectOfType<NetPlayerController>();
+        }
+    }
+
+    //[Rpc(SendTo.Owner)]
+    public void UpdateLife()
+    {
+        if (player.lifePoints.Value > 0)
+        {
+            health = player.lifePoints.Value;
+        }
+        else
+        {
+            health = 0;
+        }
+    }
+
+    public void UpdateLifeText()
+    {
+        healthText.text = "Health : " + health.ToString();
     }
 }
--- Assets/PersonalScore.cs
+++ Assets/PersonalScore.cs
@@ -1,20 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PersonalScore : MonoBehaviour
+public class PersonalScore : NetworkBehaviour
 {
-    public int score;
-    public NetPlayerController player;
+    public TextMeshProUGUI scoreText;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
